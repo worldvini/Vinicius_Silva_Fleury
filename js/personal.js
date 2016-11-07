28d0647021e60df45ea0bd3d741e20a908d6cd74
Vinicius da Silva Fleury <worldvini@hotmail.com>
2016-11-07 03:08:30 -0200

    'header_nav_name_background'

---
+++ js/personal.js
@@ -0,0 +1,22 @@
+//quando todo o documento estiver pronto
+$(document).ready(function(){
+
+	//scroll suave
+	function scrollSuave(element, speed){
+		var href = element.attr('href');
+		var top = $(href).offset().top;
+
+		$("html , body").animate({scrollTop : top}, speed);
+	}
+
+	$(function(){
+		$("#scroll a").click(function(e){
+			e.preventDefault();
+
+			scrollSuave($(this), 800);
+		});
+	});
+
+
+});
+
